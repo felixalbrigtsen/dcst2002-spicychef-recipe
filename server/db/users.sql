d675921436633e62a0f3d5fc1aad9f7205f6d758
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-10-12 15:33:48 +0200

    Fix ci directory
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Install and configure typescript
    
    Add draft for database models
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Install openssh before running deploy
    
    Update .gitlab-ci.yml file
    
    Remove ssh-agent, use key file
    
    Reinstall ssh-agent
    
    Update apt before installing
    
    Auto accept apt install
    
    Auto accept apt install, rename key var
    
    Update .gitlab-ci.yml file
    
    use ssh-add without tempfile
    
    Change to alpine deploy env
    
    Fix build command
    
    Remove merge-request-only
    
    Remove only-limit
    
    Fix missing image
    
    Fix missing tilde in path
    
    Revert to default SSH port
    
    Fix quote/variable syntax
    
    Remove keyscan
    
    Replace username variable
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Only test when something changed
    
    Only test when something changed
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Revert to port 22
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file
    
    Update .gitlab-ci.yml file

---
+++ server/db/users.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `googleid` varchar(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+
+  `isadmin` tinyint(1) NOT NULL DEFAULT '0',
+
+  `created_at` datetime NOT NULL,
+  
+  PRIMARY KEY (`googleid`)
+);
+
+DROP TABLE IF EXISTS `userlikes`;
+CREATE TABLE `userlikes` (
+  `googleid` varchar(11) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+
+  `created_at` datetime NOT NULL,
+  
+  FOREIGN KEY (`googleid`) REFERENCES `users`(`googleid`),
+  FOREIGN KEY (`recipe_id`) REFERENCES `recipes`(`id`)
+  PRIMARY KEY (`googleid`, `recipe_id`)
+);
+
+
+DROP TABLE IF EXISTS `cartItems`;
+CREATE TABLE `cartItems` (
+  `googleid` varchar(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+
+  FOREIGN KEY (`googleid`) REFERENCES `users`(`googleid`),
+  FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients`(`id`)
+  PRIMARY KEY (`googleid`, `ingredient_id`)
+);
