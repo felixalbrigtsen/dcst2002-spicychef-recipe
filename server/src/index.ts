b5ef6697afa2e36225b2c33879b453770d5db855
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-10-13 19:56:42 +0200

    Add a functioning express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,26 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import * as path from 'path';
 
-console.log(sum(1, 2));
+
+dotenv.config();
+const port: number = Number(process.env.PORT) || 3000;
+const clientBuildPath = path.join(__dirname, (process.env.CLIENT_BUILD_PATH || '/../../client/build'));
+console.log("Serving client from '" + clientBuildPath + "'");
+
+const app: Express = express();
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Serve static files from the React frontend
+app.use(express.static(clientBuildPath));
+
+// Test endpoint to check if server is running
+app.get('/api', (req, res) => {
+  res.send('Recipe Server!');
+});
+
