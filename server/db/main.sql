64820a96951e9495e85a3a266642306e81865c64
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-10-13 14:05:01 +0200

    Add draft for database models

---
+++ server/db/main.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS `recipe`;
+CREATE TABLE `recipe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `summary` text NOT NULL,
+  `instructions` text NOT NULL,
+  `servings` int(11) NOT NULL,
+  `imageurl` varchar(255),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  -- `minutes` int(11) NOT NULL,
+  -- add tags?
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `ingredient`;
+CREATE TABLE `ingredient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `unit`;
+CREATE TABLE `unit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `recipe_ingredient`;
+CREATE TABLE `recipe_ingredient` (
+  `recipe_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`recipe_id`,`ingredient_id`,`unit_id`),
+  FOREIGN KEY (`recipe_id`) REFERENCES `recipe`(`id`),
+  FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient`(`id`),
+  FOREIGN KEY (`unit_id`) REFERENCES `unit`(`id`)
+);
+
