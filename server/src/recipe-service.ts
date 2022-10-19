13364c72be1f63d90f1e551dec54dceb27352cd5
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-10-19 22:31:12 +0200

    Better service getters

--- server/src/recipe-service.ts
+++ server/src/recipe-service.ts
@@ -3,23 +3,15 @@ import type { RowDataPacket, ResultSetHeader } from 'mysql2';
 
 export type Ingredient = {
   id: number;
-  name: string;
-
+  ingredientName: string;
+  amount: number;
+  unitId: number;
+  unitName: string;
 };
 
 export type Unit = {
   id: number;
   name: string;
-};
-
-export type IngredientInstace = {
-  ingredientId: number;
-  amount: number;
-  unitId: number;
-
-  // Virtual fields
-  ingredientName: Ingredient['name'];
-  unitName: Unit['name'];
 };
 
 export type Recipe = {
@@ -31,9 +23,23 @@ export type Recipe = {
   imageUrl: string | null;
   videoUrl: string | null;
   created_at: string | null;
-  ingredients: IngredientInstace[];
+  ingredients: Ingredient[];
+  tags: string[];
 };
 
+
+/*
+Grunnleggende typer har hver sin "enkle" getter.
+  feks: getRecipe(id: number): Promise<Recipe>
+Disse gir BARE den rene dataen som er i databasen (Kun en SELECT, ingen JOINs)
+
+
+For å hente ut data som er relatert til hverandre, har vi "komplekse" getters.
+  feks getRecipeFull(id: number): Promise<Recipe>
+Disse gjør en SELECT, og JOINer på alle tabeller som er relatert til hverandre.
+I eksemplet utgjør det også alle ingrediensene med tilhørende enhet og antall, i tillegg til alle tags.
+
+*/
 
 class RecipeService {
   getRecipes(): Promise<Recipe[]> {
@@ -55,6 +61,33 @@ class RecipeService {
           return reject(err);
         }
         resolve(results[0] as Recipe);
+      });
+    });
+  }
+
+  getRecipesLikeTitle(title: string): Promise<Recipe[]> {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM recipe WHERE title LIKE ?', [`%${title}%`], (err, results: RowDataPacket[]) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results as Recipe[]);
+      });
+    });
+  }
+
+  getRecipesWithTag(tag: string): Promise<Recipe[]> {
+    return new Promise((resolve, reject) => {
+      pool.query(`
+        SELECT recipe.id, recipe.title, recipe.summary 
+        FROM recipe_tag 
+        LEFT JOIN recipe ON recipe_tag.recipeId = recipe.id 
+        WHERE recipe_tag.name = "dinner" 
+        GROUP BY recipe.id;`, [tag], (err, results: RowDataPacket[]) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results as Recipe[]);
       });
     });
   }
@@ -87,13 +120,18 @@ class RecipeService {
     });
   }
 
-  getIngredientsInRecipe(recipeId: number): Promise<IngredientInstace[]> {
+  getIngredientsInRecipe(recipeId: number): Promise<Ingredient[]> {
     return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM recipe_ingredient WHERE recipeId = ?', [recipeId], (err, results: RowDataPacket[]) => {
+      pool.query(`
+        SELECT ingredientId as id, unitId, quantity, ingredient.name AS ingredientName, unit.name AS unitName 
+        FROM recipe_ingredient 
+        LEFT JOIN unit ON recipe_ingredient.unitId = unit.id 
+        LEFT JOIN ingredient ON recipe_ingredient.ingredientId = ingredient.id 
+        WHERE recipe_ingredient.recipeId = ?`, [recipeId], (err, results: RowDataPacket[]) => {
         if (err) {
           return reject(err);
         }
-        resolve(results as IngredientInstace[]);
+        resolve(results as Ingredient[]);
       });
     });
   }
@@ -142,28 +180,27 @@ class RecipeService {
     });
   }
 
+  getTagsInRecipe(recipeId: number): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM recipe_tag WHERE recipeId = ?', [recipeId], (err, results: RowDataPacket[]) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results.map((result) => result.name));
+      });
+    });
+  }
+
   getRecipeFull(id: number): Promise<Recipe> {
     return new Promise((resolve, reject) => {
       this.getRecipe(id)
       .then(async (recipe) => {
 
-        recipe.ingredients = [];
+        let tags = await this.getTagsInRecipe(id);
+        recipe.tags = tags;
 
-        let ingredientEntries = await this.getIngredientsInRecipe(recipe.id);
-
-        // TODO: Do SQL magic instead of this
-        for (let ingredientEntry of ingredientEntries) {
-          let entry = {...ingredientEntry};
-          console.log(entry);
-
-          let ingredient = await this.getIngredient(entry.ingredientId);
-          let unit = await this.getUnit(entry.unitId);
-
-          entry.ingredientName = ingredient.name;
-          entry.unitName = unit.name;
-          
-          recipe.ingredients.push(entry);
-        }
+        let ingredients = await this.getIngredientsInRecipe(recipe.id);
+        recipe.ingredients = ingredients;
 
         resolve(recipe);
 
