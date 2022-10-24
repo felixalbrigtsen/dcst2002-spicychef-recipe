b09fda0a7efbf1ea11f837ae264ad3a9bbeb06dc
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-24 13:07:49 +0200

    Updated imports for page components, Fixes #13

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,13 @@ import {
   Routes 
 } from 'react-router-dom';
 
-import Home from './Frontpage';
-
-function SearchPage () {
-  return <h1>Search</h1>;
-}
-
-function LoginPage () {
-  return <h1>Login</h1>;
-}
+import Home from './pages/Frontpage';
+import SearchPage from './pages/SearchPage';
+import LoginPage from './pages/LoginPage';
+import RecipePage from './pages/RecipePage';
+import IngredientPage from './pages/IngredientPage';
+import ShoppingCartPage from './pages/ShoppingCartPage';
+import LikePage from './pages/LikePage';
 
 function App() {
   return (
@@ -23,6 +21,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/recipes" element={<RecipePage />} />
+        <Route path="/ingredients" element={<IngredientPage />} />
+        <Route path="/cart" element={<ShoppingCartPage />} />
+        <Route path="/likes" element={<LikePage />} />
       </Routes>
     </Router>
   </>
