7ef8dcef5f537608d0b78ede1b8762b908ea1c9c
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-24 13:08:31 +0200

    Created Ingredient Card, Fixes #13

---
+++ client/src/components/IngredientCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import { Button, Card, Image, Media, Heading, Content, Icon } from 'react-bulma-components';
+import {useState} from 'react';
+import { FaArrowRight, FaThumbsUp } from 'react-icons/fa';
+
+function IngredientCard() {
+    
+}
