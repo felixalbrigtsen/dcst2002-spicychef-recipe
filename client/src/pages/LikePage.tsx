ef4e4eb217e573a4c6f74bdd4e06e8ddd353fdaf
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-24 13:07:17 +0200

    Created Likes Page, Fixes #13

---
+++ client/src/pages/LikePage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes 
+} from 'react-router-dom';
+
+export default function LikePage() {
+    return (
+        <div>
+        <h1>My Likes</h1>
+        </div>
+    );
+}
