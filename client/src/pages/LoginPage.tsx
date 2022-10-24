a91a6bff66c5bced6156b127443e0a69fa4bd9d6
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-24 13:07:08 +0200

    Created Login Page, Fixes #13

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes 
+} from 'react-router-dom';
+
+export default function LoginPage() {
+    return (
+        <div>
+        <h1>Login</h1>
+        </div>
+    );
+}
