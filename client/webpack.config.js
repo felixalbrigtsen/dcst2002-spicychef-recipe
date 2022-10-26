5126a24ec2e5858bd08c8bfc55117081e5c86a26
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-10-26 15:33:31 +0200

    Replace react-scripts with webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,6 +8,7 @@
  */
 
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.tsx', // Initial file to bundle
@@ -21,6 +22,16 @@ module.exports = {
   },
   // Makes original source code available to the browser for easier identification of error causes.
   devtool: 'source-map',
+  devServer: {
+    // Serve files from the public directory
+    static: { directory: path.join(__dirname, 'public') },
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    // Load environment variables from .env file
+    new Dotenv(),
+  ],
   module: {
     rules: [
       {
