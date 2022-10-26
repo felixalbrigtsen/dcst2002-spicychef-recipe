baf21e530e0ba8ced41d6f4bcd1bafb95cae2f63
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-26 12:26:39 +0200

    Recipe List Page created #18

---
+++ client/src/pages/RecipeListPage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes 
+} from 'react-router-dom';
+
+export default function RecipeList() {
+    return (
+        <div>
+        <h1>Recipe</h1>
+        </div>
+    );
+}
