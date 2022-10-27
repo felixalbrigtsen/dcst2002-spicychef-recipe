9eb1dc0f2d4eba05405fb4a0c507a3b542a69d10
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-27 14:48:01 +0200

    Functional recipe list with working "grid" #18

--- client/src/pages/RecipeListPage.tsx
+++ client/src/pages/RecipeListPage.tsx
@@ -5,10 +5,30 @@ import {
   Routes 
 } from 'react-router-dom';
 
+import RecipeCard from '../components/RecipeCard';
+import recipeService from '../services/recipe-service';
+import { Recipe } from '../models/Recipe';
+
+import { Columns } from 'react-bulma-components';
+
 export default function RecipeList() {
+    let [ recipeList, setRecipeList ] = React.useState<Recipe[]>([]);
+
+    React.useEffect(() => {
+        recipeService.getRecipesShort()
+        .then(data => {
+            setRecipeList(data);
+        })
+    }, []);
     return (
-        <div>
-        <h1>Recipe</h1>
-        </div>
+        <>
+          <Columns className="is-multiline">
+          {recipeList.map((recipe) => (
+            <Columns.Column>
+              <RecipeCard recipe={recipe} />
+            </Columns.Column>
+          ))}
+          </Columns>
+        </>
     );
 }
