1be96bda690a11b9a9d72425ac431d0481f68d62
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-27 14:47:46 +0200

    Added props to render recipes #18

--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -4,28 +4,35 @@ import { Button, Card, Image, Media, Heading, Content, Icon } from 'react-bulma-
 import {useState} from 'react';
 import { FaArrowRight, FaThumbsUp } from 'react-icons/fa';
 
-function RecipeCard() {
+import recipeService from '../services/recipe-service';
+import { Recipe } from '../models/Recipe';
+
+interface RecipeCardProps {
+    recipe: Recipe;
+}
+
+function RecipeCard(props: RecipeCardProps) {
+
     return(
         <>
+        {/* TODO: Fix standard card height */}
             <Card style={{ width: 300, margin: 'auto' }}>
-                <Card.Image size="4by3" src="https://cdn.discordapp.com/attachments/894239765430419549/1032295931435036772/unknown.png" />
+                <Card.Image size="4by3" src={props.recipe.imageUrl} />
                 <Card.Content>
                     <Media>
                         <Media.Item>
-                            <Image size={64} alt="64x64" src="https://media.discordapp.net/attachments/894239765430419549/1032296471632019466/unknown.png" />
-                        </Media.Item>
-                        <Media.Item>
                             <Heading size={4}>
-                                Brunost
+                                {props.recipe.title}
                             </Heading>
                             <Heading subtitle size={6}>
-                                @Norwegian
+                                {props.recipe.tags.map((tag) => (
+                                    <span className="tag is-info">{tag}</span>
+                                ))}
                             </Heading>
                         </Media.Item>
                     </Media>
                     <Content>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Phasellus nec iaculis mauris.
+                        {props.recipe.summary}
                     </Content>
                 </Card.Content>
                 <Card.Footer>
@@ -37,13 +44,11 @@ function RecipeCard() {
                             </Icon>
                         </Button>
                     </Card.Footer.Item>
-                    <Card.Footer.Item renderAs="a" href="/recipe/{id}">
-                        Read more 
-                        <br />
-                        <Icon>
-                            <FaArrowRight />
-                        </Icon>
-                    </Card.Footer.Item>
+                    <a href={`/recipe/${props.recipe.id}`}>
+                        <Card.Footer.Item>
+                            Read more <FaArrowRight />
+                        </Card.Footer.Item>
+                    </a>
                 </Card.Footer>
             </Card>
         </>
