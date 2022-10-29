ee595fc2cd01e677d0a8b96035de47dc745f7fc7
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-10-29 16:46:58 +0200

    Basic setup of recipe page, #18

--- client/src/pages/RecipePage.tsx
+++ client/src/pages/RecipePage.tsx
@@ -7,7 +7,7 @@ import {
 
 import { useParams } from 'react-router-dom'
 
-import { Card, Container, Image, Media } from 'react-bulma-components'
+import { Container, Image, Media, Tile, Heading, Notification, Button, Form } from 'react-bulma-components'
 
 import recipeService from '../services/recipe-service';
 import { Recipe } from '../models/Recipe';
@@ -15,24 +15,71 @@ import { Recipe } from '../models/Recipe';
 
 function RecipePage() {
 
-    let [ recipe, setRecipe ] = React.useState<Recipe>();
+    let [ recipe, setRecipe ] = React.useState<Recipe>([]);
 
     let id = Number(useParams().id);
     React.useEffect(() => {
         recipeService.getRecipe(id)
         .then(data => {setRecipe(data)});
     }, []);
-    
+
 
     return (
         <>
         <Container>
-          <Card className='has-text-centered'>
-            <Card.Header>
-            </Card.Header>
-            <Card.Content>
-            </Card.Content>
-          </Card>
+        <Tile kind="ancestor">
+          <Tile vertical>
+            {/* TODO: Make the recipe page look better */}
+            <Tile kind="parent" renderAs={Notification}>
+              <Tile kind="child" renderAs={Media} object>
+                <Media.Item renderAs="figure">
+                  <Image size={128} alt="64x64" src={recipe.imageUrl} />
+                </Media.Item>
+                <Media.Item>
+                  <Heading size={4}>{recipe.title}</Heading>
+                  <Heading subtitle size={6}>{recipe.summary}</Heading>
+                </Media.Item>
+                <Media.Item>
+                </Media.Item>
+                <Media.Item>
+                  <a href={recipe.videoUrl} target="_blank">
+                  <Heading subtitle size={6}>Watch the video</Heading>
+                  <Image size={128} alt="64x64" src="https://i.imgur.com/raE8eQy.png" />
+                  </a>
+                </Media.Item>
+              </Tile>
+              <Tile kind="child" size={8}>
+                <Heading subtitle size={4}>Instructions</Heading>
+                <Heading subtitle size={6}>{recipe.instructions}</Heading>
+              </Tile>
+            </Tile>
+            <Tile kind="parent" renderAs={Notification}>
+            {/* TODO: Scale ingredient.amount with servings */}
+            <Tile kind="child">
+              <Heading subtitle size={4}>Ingredients</Heading>
+              { recipe.ingredients?.map((ingredient) => {
+                  return (
+                    <Heading key={ingredient.id} subtitle size={6}>{ingredient.amount} {ingredient.unitId} {ingredient.unitName} {ingredient.ingredientName} </Heading>
+                  )
+                })
+              }
+            </Tile>
+            <Tile kind="child">
+                {recipe.tags?.map((tag) => (
+                  <Button key={tag}>{tag}</Button>
+                ))}
+                  <Form.Field>
+                    <Form.Label>Servings:</Form.Label>
+                    <Form.Control>
+                      {/* TODO: Add functionality to servings */}
+                      <Form.Input type="number" placeholder="Servings" defaultValue={recipe.servings} min={1}
+                      onChange={(event) => console.log(event?.currentTarget.value)} />
+                    </Form.Control>
+                  </Form.Field>
+              </Tile>
+            </Tile>
+          </Tile>
+        </Tile>
         </Container>
         </>
     );
