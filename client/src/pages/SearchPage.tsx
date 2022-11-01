025886dd3226597ebb29fdba5600a35c0f40dac4
Kristoffer Longva Eriksen <kriloneri@gmail.com>
2022-11-01 17:23:43 +0100

    Added single tab to search page

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,34 +1,73 @@
 import * as React from 'react';
-import { Heading, Hero, Tile, Tabs } from 'react-bulma-components';
+import { Heading, Hero, Tile, Tabs, Box} from 'react-bulma-components';
 import { 
   BrowserRouter as Router, 
   Route, 
   Routes 
 } from 'react-router-dom';
+import { Recipe } from '../models/Recipe';
 
+type Tab = {id: number, name: string, recipes: Recipe[]}
+
+interface TabContent {
+  tabContent: Tab
+}
+
+function SingleTab(props : TabContent) {
+  return (
+    <>
+      <Heading>{props.tabContent.name}</Heading>
+        {props.tabContent.recipes.map((recipe) =>
+        <>
+          <Box>{recipe.title}</Box>
+          <Box>{recipe.summary}</Box>
+        </>
+        )}
+    </>
+  )
+}
+
+interface TabList {
+  tabList: Tab[]
+}
+
+function SearchTabs(props : TabList) {
+  let activeTab : number = 1
+
+  const changeActiveTab = (id: number) => {
+    activeTab = id
+  }
+
+  return (
+    <>
+      {props.tabList.map((tab) => {
+        <Tabs.Tab onClick={() => changeActiveTab(tab.id)} active={activeTab==tab.id}><SingleTab tabContent = {tab}/></Tabs.Tab>
+      })}
+    </>
+  )
+}
 
 export default function SearchPage() {
-  let activeTab = 1
-  const tabList : {id: number, name: string, results: string}[] = [
+  const tabList : Tab[] = [
     {
       id: 1,
-      name: "Recipes",
-      results: "Stuff 1"
-    }, {
-      id: 2,
-      name: "Ingredients",
-      results: "Stuff 2"
+      name: "Oppskrifter",
+      recipes: []
+    },
+    {
+      id: 1,
+      name: "Ingredienser",
+      recipes: []
     }
-  ];
+  ]
 
   return (
     <Hero>
       <Hero.Body>
         <Heading>Search</Heading>
         <Tile>
-          <Tabs>
-            <Tabs.Tab><a>Oppskrifter</a></Tabs.Tab>
-            <Tabs.Tab active={true}><a>Ingredienser</a></Tabs.Tab>
+          <Tabs>         
+             <SearchTabs tabList = {tabList}/>
           </Tabs>
         </Tile>
       </Hero.Body>
