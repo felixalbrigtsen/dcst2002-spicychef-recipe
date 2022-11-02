f5f6aed4e5a90e3268a8524b771ab51f869ade22
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-02 16:19:36 +0100

    ScrollUp Button as own component

---
+++ client/src/components/ScrollUp.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes 
+} from 'react-router-dom';
+
+import { useState } from 'react';
+
+import { FaArrowCircleUp } from 'react-icons/fa';
+
+function ScrollButton() {
+    const [visible, setVisible] = useState(false)
+    
+    const toggleVisible = () => {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 50){
+        setVisible(true)
+      } 
+      else if (scrolled <= 300){
+        setVisible(false)
+      }
+    };
+    
+    const scrollToTop = () =>{
+      window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+        /* you can also use 'auto' behaviour
+           in place of 'smooth' */
+      });
+    };
+  
+    window.addEventListener('scroll', toggleVisible);
+    
+    return (
+       <FaArrowCircleUp 
+        onClick={scrollToTop}
+        style={{
+          position: 'fixed',
+          bottom: '1.5rem',
+          left: '.5rem',
+          fontSize: '2rem',
+          zIndex: 1,
+          display: visible ? 'inline' : 'none'
+        }} />
+    );
+  }
+
+export default ScrollButton;
