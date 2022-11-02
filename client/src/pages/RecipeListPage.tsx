dc9a8d355a1a7782891585b9378f9064891f2caa
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-02 14:32:19 +0100

    Added button to scroll to top

--- client/src/pages/RecipeListPage.tsx
+++ client/src/pages/RecipeListPage.tsx
@@ -5,11 +5,52 @@ import {
   Routes 
 } from 'react-router-dom';
 
+import { useState } from 'react';
+
 import RecipeCard from '../components/RecipeCard';
 import recipeService from '../services/recipe-service';
 import { Recipe } from '../models/Recipe';
 
-import { Columns, Box } from 'react-bulma-components';
+import { Columns, Box, Button } from 'react-bulma-components';
+import { FaArrowCircleUp } from 'react-icons/fa';
+
+function ScrollButton() {
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    } 
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
+  
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+  
+  return (
+     <FaArrowCircleUp 
+      onClick={scrollToTop}
+      style={{
+        position: 'fixed',
+        bottom: '2rem',
+        left: '90%',
+        fontSize: '2.5rem',
+        zIndex: 1,
+        display: visible ? 'inline' : 'none'
+      }} />
+  );
+}
 
 export default function RecipeList() {
     let [ recipeList, setRecipeList ] = React.useState<Recipe[]>([]);
@@ -22,11 +63,12 @@ export default function RecipeList() {
         <>
           <Columns className="is-multiline is-centered" style={{marginTop: '2rem', marginLeft: 'auto', marginRight: 'auto'}}>
           {recipeList.map((recipe) => (
-            <Columns.Column className='is-narrow' key={recipe.id}>
+            <Columns.Column className='is-one-quarter' key={recipe.id}>
               <RecipeCard recipe={recipe} />
             </Columns.Column>
           ))}
           </Columns>
+          <ScrollButton />
         </>
     );
 }
