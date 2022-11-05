3c2c2491ff92b0eccee9968a9ae7623afea37a15
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-05 11:54:20 +0100

    Created edit page with frontend functionality #18

--- client/src/pages/EditRecipePage.tsx
+++ client/src/pages/EditRecipePage.tsx
@@ -6,11 +6,32 @@ import {
   Link
 } from 'react-router-dom';
 
+import { Form, Button, Container, Tile, Hero } from 'react-bulma-components';
+
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import { Recipe } from '../models/Recipe';
+import recipeService from '../services/recipe-service';
+import RecipeForm from '../components/RecipeForm';
+
 function EditRecipe () {
+
+  let [ recipe, setRecipe ] = React.useState<Recipe>({id: 0, title: "", summary: "", instructions: "", servings: 0, imageUrl: "", videoUrl: "", created_at: "", ingredients: [], tags: []});
+
+    let id = Number(useParams().id);
+    React.useEffect(() => {
+        recipeService.getRecipe(id)
+        .then(data => {setRecipe(data)});
+    }, []);
+
     return (
-        <div>
-        <h1>Edit Recipe</h1>
-        </div>
+      <Container>
+      <Hero>
+        <Hero.Body>
+          <RecipeForm recipe={recipe} />
+        </Hero.Body>
+      </Hero>
+      </Container>
     );
 }
 
