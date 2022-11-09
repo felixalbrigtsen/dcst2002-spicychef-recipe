6342e8d7bf0ab13cfb800885d8550938396a7f3f
Jonas Haugland <jonashaugland@hotmail.com>
2022-11-09 15:11:08 +0100

    Clean up shopping list
    Co-authored-by: krloer <krloer@users.noreply.github.com>
    Co-authored-by: Felix Albrigtsen <felixalbrigtsen@gmail.com>

---
+++ client/src/models/RecipeIngredient.ts
@@ -0,0 +1,23 @@
+/**
+ * Ingredient
+ * @alias RecipeIngredient
+ *
+ */
+export type RecipeIngredient = {
+  id: number;
+  ingredientName: string;
+
+  /**
+   * Number of units of the ingredient
+   * @example 1
+   */
+  quantity: number;
+  /**
+   * Unit ID from the table recipe_ingredient 
+   */
+  unitId: number;
+  /**
+   * Name of unit (dl, g, tbsp. etc.)
+   */
+  unitName: string;
+};
