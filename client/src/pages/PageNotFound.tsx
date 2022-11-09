f9c563fe692da3b654ce79e34ac303a96182cb3f
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-09 17:59:19 +0100

    Added 404 page

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Tile, Notification, Container, Heading, Image, Button } from 'react-bulma-components';
 
 export default function PageNotFound() {
     return (
-        <div>
-            <h1>Page Not Found</h1>
-        </div>
-    );
+    <Container className='mt-2'>
+            <Tile className='is-vertical has-text-centered' color='info' renderAs={Notification}>
+                    <Heading>404 - Page Not Found</Heading>
+            </Tile>
+            <Tile className='is-vertical has-text-centered is-align-items-center'>
+                <Heading subtitle>Sorry, the page you are looking for does not exist.</Heading>
+                <Image style={{width: '25%', margin:'1rem'}} src='/404.gif' />
+                <Link to="/"><Button outlined className='is-rounded' color='link'><Heading subtitle>Go home!</Heading></Button></Link>
+            </Tile>
+    </Container>
+    ); 
 }
