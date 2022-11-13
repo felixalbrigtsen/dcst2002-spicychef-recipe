1288a3ba280e22165323b99c6f1ecb811d13ffdc
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-11-13 15:01:37 +0100

    Allow initdb to be used in the test functions

--- server/src/utils/initdb.ts
+++ server/src/utils/initdb.ts
@@ -13,17 +13,21 @@ const sqlFilesContent = sqlFiles.map((file) => {
   return readFileSync(sql_file_path + file, 'utf8').toString().replace(/(\r|\n)/g, ' ');
 });
 
-dotenv.config();
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  connectionLimit: 4,
-  multipleStatements: true,
-  typeCast: (field, next) =>
-    field.type == 'TINY' && field.length == 1 ? field.string() == '1' : next(),
-});
+let pool: mysql.Pool;
+
+function setupPool(envpath: string) {
+  dotenv.config({ path: envpath });
+  pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    connectionLimit: 4,
+    multipleStatements: true,
+    typeCast: (field, next) =>
+      field.type == 'TINY' && field.length == 1 ? field.string() == '1' : next(),
+  });
+}
 
 function runQuery(sql: string):  Promise<any> {
   return new Promise((resolve, reject) => {
@@ -36,7 +40,7 @@ function runQuery(sql: string):  Promise<any> {
   });
 }
 
-async function main() {
+async function execFiles() {
   for (const [ i, sql ] of sqlFilesContent.entries()) {
     console.log(`Executing ${sqlFiles[i]}...`);
     try {
@@ -51,23 +55,40 @@ async function main() {
   console.log(" --- Done --- ");
 }
 
-console.log("Operating on database " + process.env.MYSQL_DATABASE + " on host " + process.env.MYSQL_HOST);
+async function main() {
+  setupPool('./.env');
+  console.log("Operating on database " + process.env.MYSQL_DATABASE + " on host " + process.env.MYSQL_HOST);
 
-console.log("Loaded SQL files: " + sqlFiles.join(', '));
+  console.log("Loaded SQL files: " + sqlFiles.join(', '));
 
-console.log('This will delete all data in the database. Are you sure? (y/n)');
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', async function (inputBuffer) {
-  let text = inputBuffer.toString().trim();
+  console.log('This will delete all data in the database. Are you sure? (y/n)');
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', async function (inputBuffer) {
+    let text = inputBuffer.toString().trim();
 
-  if (text === 'y') {
-    await main();
-  } else {
-    console.log('Aborted');
-  }
-  console.log('Exiting...');
-  process.exit();
-}); 
+    if (text === 'y') {
+      await execFiles();
+    } else {
+      console.log('Aborted');
+    }
+    console.log('Exiting...');
+    process.exit();
+  }); 
+}
 
 
+async function initTest() {
+  setupPool('./.env.test');
+  await execFiles();
+}
+
+// if main flag is set, run main function
+if (process.argv[2] === 'main') {
+  main();
+}
+// Otherwise, this module can be imported and the test functions can be called
+
+module.exports = {
+  initTest,
+};
