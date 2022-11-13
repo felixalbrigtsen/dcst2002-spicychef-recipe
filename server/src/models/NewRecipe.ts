df862fc187b2cdf6b6a71735836f204871ffb53a
Kristoffer Longva Eriksen <kriloneri@gmail.com>
2022-11-13 11:00:34 +0100

    Added some more data and the NewRecipe model

---
+++ server/src/models/NewRecipe.ts
@@ -0,0 +1,18 @@
+/**
+ * NewRecipe 
+ * @alias NewRecipe
+ *
+ */
+ export type NewRecipe = {
+    id: number;
+    title: string;
+    summary: string; 
+    servings: number; 
+    instructions: string; 
+    imageUrl: string;
+    videoUrl: string;
+    ingredients: {ingredientName: string, quantity: number, unitName: string}[];
+    tags: string[];
+  };
+
+// TODO: JsDoc this
--- server/src/test/recipe-router.test.ts
+++ server/src/test/recipe-router.test.ts
@@ -4,12 +4,43 @@ import app from '..';
 import { Recipe } from '../models/Recipe';
 import { Ingredient } from '../models/Ingredient';
 import recipeService from '../services/recipe-service';
+import { NewRecipe } from '../models/NewRecipe';
 
 const port = Number(process.env.PORT)
 
-const testData: Recipe[] = [
+const testRecipes: Recipe[] = [
     {"id":1,"title":"Tunisian Lamb Soup","summary":"Meal from MealDB","instructions":"Add the lamb to a casserole and cook over high heat. When browned, remove from the heat and set aside.", "servings":2,"imageUrl":"https://www.themealdb.com/images/media/meals/t8mn9g1560460231.jpg","videoUrl":"https://www.youtube.com/watch?v=w1qgTQmLRe4","created_at":"2022-11-02T19:23:43.000Z","likes":0,"tags":["Lamb","Soup","Tunisian"],"ingredients": [{"id":1,"unitId":1,"quantity":500,"ingredientName":"Lamb Mince","unitName":"g"},{"id":2,"unitId":2,"quantity":2,"ingredientName":"Garlic","unitName":"cloves minced"}]}
 ];
+
+const testNewRecipes: NewRecipe[] = [
+    {"id": -1, "title": "Lasagna","summary": "","servings": 2,"instructions": "Garfield","imageUrl": "https://static.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg/","videoUrl": "https://www.youtube.com/watch?v=qvc4DMiioRc","ingredients": [{"ingredientName": "Onion","quantity": 0.5,"unitName": "units"},{"ingredientName": "Tomato Puree","quantity": 2,"unitName": "g"},{"ingredientName": "Garlic","quantity": 1,"unitName": "clove"}],"tags": ["Italian"]},
+    {
+        "id": -1,
+        "title": "Chicken Soup",
+        "summary": "",
+        "servings": 4,
+        "instructions": "Boil",
+        "imageUrl": "https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg",
+        "videoUrl": "https://www.youtube.com/watch?v=rX184PQ1UMI",
+        "ingredients": [
+          {
+            "ingredientName": "Chicken Stock",
+            "quantity": 0,
+            "unitName": ""
+          },
+          {
+            "ingredientName": "Chicken",
+            "quantity": 0,
+            "unitName": ""
+          }
+        ],
+        "tags": [
+          "Chicken"
+        ]
+      }
+    ]
+
+
 
 // Since API is not compatible with v1, API version is increased to v2
 axios.defaults.baseURL = 'http://localhost:3001/api/v2';
@@ -27,10 +58,10 @@ beforeEach((done) => {
 
     // Create testTasks sequentially in order to set correct id, and call done() when finished
     recipeService
-      .addRecipe(testData[0].title, testData[0].summary, testData[0].instructions, testData[0].servings, testData[0].imageUrl, testData[0].videoUrl)
-      .then(() => recipeService.addRecipe() // Create testTask[1] after testTask[0] has been created
-      .then(() => recipeService.addRecipe() // Create testTask[2] after testTask[1] has been created
-      .then(() => done()); // Call done() after testTask[2] has been created
+      .addRecipe(testNewRecipes[0].title, testNewRecipes[0].summary, testNewRecipes[0].instructions, testNewRecipes[0].servings, testNewRecipes[0].imageUrl, testNewRecipes[0].videoUrl)
+      .then(() => recipeService.addRecipe(testNewRecipes[1].title, testNewRecipes[1].summary, testNewRecipes[1].instructions, testNewRecipes[1].servings, testNewRecipes[1].imageUrl, testNewRecipes[1].videoUrl) // Create testTask[1] after testTask[0] has been created
+      .then(() => recipeService.addRecipe(testNewRecipes[2].title, testNewRecipes[2].summary, testNewRecipes[2].instructions, testNewRecipes[2].servings, testNewRecipes[2].imageUrl, testNewRecipes[2].videoUrl) // Create testTask[2] after testTask[1] has been created
+      .then(() => done())));
   });
 });
 
