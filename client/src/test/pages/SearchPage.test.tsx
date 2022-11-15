4417fb015687a93a4a18e64df4c8c992551e69fa
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-15 21:41:24 +0100

    Basic tests, need to do more advanced ones later

--- client/src/test/pages/SearchPage.test.tsx
+++ client/src/test/pages/SearchPage.test.tsx
@@ -7,3 +7,36 @@ import {
 } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
+import selectEvent from 'react-select-event';
+import SearchPage from '../../pages/SearchPage';
+
+describe('SearchPage stuff', () => {
+  test('renders SearchPage', () => {
+    render(<SearchPage />);
+    expect(screen.getByRole('heading', {name: "Search"})).toHaveTextContent('Search');
+    expect(screen.getByText('Tags')).toBeInTheDocument();
+    expect(screen.getByText('Ingredients')).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: "Search"})).toHaveTextContent('Search');
+    expect(screen.getByRole('combobox', {name: "Ingredients"})).toBeInTheDocument();
+    expect(screen.getByRole('combobox', {name: "Tags"})).toBeInTheDocument();
+  });
+  test.skip('Search button works', () => {
+    
+  });
+  test.skip('Select tags works', () => {
+
+  })
+
+  test.skip('Remove tags work', () => {
+
+  })
+
+  test.skip('Select ingredients works', () => {
+
+  })
+
+  test.skip('Remove ingredients work', () => {
+
+  })
+
+});
