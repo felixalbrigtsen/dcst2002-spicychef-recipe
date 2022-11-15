1c9a8dc9d39e25ed51099cb32f94678c90a06706
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-15 13:44:54 +0100

    Better tests for scrollUp, need scrollTo #30

--- client/src/test/components/scrollUp.test.tsx
+++ client/src/test/components/scrollUp.test.tsx
@@ -5,24 +5,27 @@ import {
   Routes, 
   Link
 } from 'react-router-dom';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent, getAllByLabelText, getByLabelText } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import ScrollButton from '../../components/ScrollUp'
 
 describe('Dynamic ScrollButton test', () => {
-    test.skip('Test visibility', () => {
+    test('Test visibility', () => {
         const {getByRole} = render(<ScrollButton/>)
-
-        let icon = getByRole("button")
-
+        let icon = getByRole("button", {hidden: true})
         expect(icon).not.toBeVisible
 
-        fireEvent.scroll(window, { target: { scrollY: 100 } })
-
+        fireEvent.scroll(window, {target: {scrollY: 300}})
         expect(icon).toBeVisible
+    })
+    test.skip('Test scroll', () => {
+        fireEvent.scroll(window, {target: {scrollY: 300}})
 
+        const {getByRole} = render(<ScrollButton/>)
+        let icon = getByRole("button", {hidden: true})
+        
+        expect(icon).toBeVisible
         fireEvent.click(icon)
-
-        expect(icon).not.toBeVisible
+        expect(window.scrollY).toBe(0);
     })
 })
