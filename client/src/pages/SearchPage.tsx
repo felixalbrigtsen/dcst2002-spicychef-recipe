7d2203845cdeadfb719e1511296d6a562df27bac
Jonas Haugland <jonashaugland@hotmail.com>
2022-11-15 17:03:31 +0100

    Broke the searchPage, needs rebuild, Jonas & Felix

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -8,7 +8,8 @@ import {
   Route, 
   Routes,
   Link, 
-  useParams
+  useParams,
+  useSearchParams
 } from 'react-router-dom';
 import { Recipe } from '../models/Recipe';
 import recipeService from '../services/recipe-service'
@@ -16,24 +17,44 @@ import RecipeCard from '../components/RecipeCard';
 import ingredientService from '../services/ingredient-service';
 import { Ingredient } from '../models/Ingredient';
 import { FaTimes } from 'react-icons/fa';
+import { stringify } from 'querystring';
 
 const animatedComponents = makeAnimated();
 
 export default function SearchPage() {
-  let {query} = useParams()
-  const [recipes, setRecipes] = React.useState<Recipe[]>([])
-  const [newQuery, setNewQuery] = React.useState<string>("")
-  const [ingredients, setIngredients] = React.useState<{"value": number, "label": string}[]>([])
-  const [ tags, setTags ] = React.useState<{"value": string, "label": string}[]>([])
+  const [ searchParams, setSearchParams ] = useSearchParams();
+  // console.log(Object.fromEntries([...searchParams]));
+  // let query = searchParams.get("query") || '';
+
+  const [recipes, setRecipes] = React.useState<Recipe[]>([]);
+  const [newQuery, setNewQuery] = React.useState<string>(searchParams.get("query") || '');
+  const [ingredients, setIngredients] = React.useState<{"value": number, "label": string}[]>(
+    searchParams.get("ingredients") ? JSON.parse(searchParams.get("ingredients") || '') : []
+  );
+  const [tags, setTags] = React.useState<{"value": string, "label": string}[]>(
+    searchParams.get("tags") ? JSON.parse(searchParams.get("tags") || '') : []
+  );
+  let [selectedIngredients, setSelectedIngredients] = React.useState<Ingredient[]>([]);
+  let [selectedTags, setSelectedTags] = React.useState<{"value": string, "label": string}[]>([]);
 
   React.useEffect(() => {
     setRecipes([])
-    recipeService.search(query)
+    recipeService.search(newQuery)
     .then(res => {
-      console.log(res)
-      setRecipes(res)})
+      let passing = res; 
+      passing = passing.filter(recipe => {
+        let hasAllIngredients = true;
+        selectedIngredients.forEach(ingredient => {
+          if (!recipe.ingredients.some(ing => ing.id === ingredient.id)) {
+            hasAllIngredients = false;
+          }
+        })
+        return hasAllIngredients;
+      });
+      setRecipes(passing);
+    })
   }, [])
-
+  
   React.useEffect(() => {
     recipeService.getRecipesShort()
     .then(res => {
@@ -42,9 +63,7 @@ export default function SearchPage() {
       let tagObjects = uniqueTags.map(t => {return {"value": t, "label": t}})
       setTags(tagObjects)
     })
-  }, [])
 
-  React.useEffect(() => {
     ingredientService.getIngredients()
     .then(res => {
       setIngredients(res.map((ingredient: Ingredient) => {
@@ -52,6 +71,10 @@ export default function SearchPage() {
       }))
     })
   }, [])
+
+  const search = function() {
+    window.location.href = `/search/?q=${newQuery}&tags=${selectedTags.join(",")}&ingredients=${selectedIngredients.join(",")}`
+  }
 
   return (
     <Hero>
@@ -61,33 +84,43 @@ export default function SearchPage() {
           <Tile kind="parent" vertical size={3}>
           <Heading>Search</Heading>
           <Form.Field>
-          <Form.Input type="text" onChange={(event) => setNewQuery(event.currentTarget.value)} 
+          <Form.Input type="text" onChange={(event: React.FormEvent) => setNewQuery((event.target as HTMLInputElement).value)} placeholder="Search for recipes" value={newQuery}
           onKeyDown={
-            (event) => {
-                if (event.key === "Enter") {
-                    window.location.href = `/search/${newQuery}`
-                }
-              }}/> 
+            (event: React.FormEvent) => {
+              if (event.key === "Enter") {
+                search()
+              }
+            }}/>
           </Form.Field>
           <Form.Field>
           <Form.Label>Tags</Form.Label>
                 <Form.Control>
-                  <Select isMulti placeholder="Recipe Tags" components={animatedComponents} name={"Tags"} options={tags} />
+                  <Select isMulti placeholder="Recipe Tags" components={animatedComponents} name={"Tags"} options={tags}
+                  onChange={(event: React.ChangeEvent) =>  {
+                    setSelectedTags((event.target as HTMLInputElement).value.map((tag) => tag.value));
+                  }}
+                  />
                 </Form.Control>
           </Form.Field>
           <Form.Field>
           <Form.Label>Ingredients</Form.Label>
                 <Form.Control>
-                  <Select isMulti placeholder="Ingredients" components={animatedComponents} options={ingredients} name={"Ingredients"} />
+                  <Select isMulti placeholder="Ingredients" components={animatedComponents} options={ingredients} name={"Ingredients"}
+                  onChange={(event: InputEvent) => {
+                    console.log(event.target)
+                    setSelectedIngredients(event.target.value.map((ingredient) => ingredient.value));
+                    console.log(selectedIngredients)
+                  }}
+                  />
                 </Form.Control>
           </Form.Field>
           {/* TODO: Make searches work with the values of the selects above; ingredients and tags */}
           {/* TODO: Make the search stay, unless you clicked one of the options, if we do that */}
           {/* Perhaps make a place where you can select tags */}
-          <Button onClick={() => window.location.href = "/search/" + newQuery}>Search</Button>
+          <Button onClick={search}>Search</Button>
           </Tile>
           <Tile kind="parent">
-          <Columns>      
+          <Columns>
             {recipes.map((recipe) => 
               <Columns.Column key={recipe.id} className='is-narrow'>
                 <RecipeCard recipe={recipe} />
