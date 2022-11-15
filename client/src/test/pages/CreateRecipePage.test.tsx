07001138d5701261df21e1a559713fd2a2678f83
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-15 21:17:53 +0100

    Created base test pages, some blocked by #39

---
+++ client/src/test/pages/CreateRecipePage.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes, 
+  Link
+} from 'react-router-dom';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
