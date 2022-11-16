44b363ffb98784232ec2588e4e0e28597466ecfd
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-16 12:31:10 +0100

    Almost done scrollup test #30

--- client/src/test/components/scrollUp.test.tsx
+++ client/src/test/components/scrollUp.test.tsx
@@ -9,7 +9,15 @@ import { render, screen, waitFor, fireEvent, getAllByLabelText, getByLabelText }
 import '@testing-library/jest-dom'
 import ScrollButton from '../../components/ScrollUp'
 
+const spyScrollTo = jest.fn();
+
 describe('Dynamic ScrollButton test', () => {
+    beforeEach(() => {
+        Object.defineProperty(global.window, 'scrollTo', { value: spyScrollTo });
+        Object.defineProperty(global.window, 'scrollY', { value: 1 });
+        spyScrollTo.mockClear();
+    });
+
     test('Test visibility', () => {
         const {getByRole} = render(<ScrollButton/>)
         let button = getByRole("button", {hidden: true})
@@ -17,8 +25,9 @@ describe('Dynamic ScrollButton test', () => {
 
         fireEvent.scroll(window, {target: {scrollY: 300}})
         expect(button).toBeVisible
-    })
-    test.skip('Test scroll', () => {
+    });
+
+    test('Test scroll', () => {
         fireEvent.scroll(window, {target: {scrollY: 300}})
 
         const {getByRole} = render(<ScrollButton/>)
@@ -26,6 +35,9 @@ describe('Dynamic ScrollButton test', () => {
         
         expect(button).toBeVisible
         fireEvent.click(button)
-        expect(window.scrollY).toBe(0);
-    })
-})
+        expect(spyScrollTo).toHaveBeenCalledWith({
+            top: 0,
+            behavior: 'smooth',
+        });
+    });
+});
