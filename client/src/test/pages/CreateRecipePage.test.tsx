cbbe5fb1f3f6eac3b3167e42a4973e3c02843d76
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-16 12:31:44 +0100

    100% coverage #30

--- client/src/test/pages/CreateRecipePage.test.tsx
+++ client/src/test/pages/CreateRecipePage.test.tsx
@@ -7,3 +7,11 @@ import {
 } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
+
+import CreateRecipePage from '../../pages/CreateRecipePage';
+
+describe('CreateRecipePage test', () => {
+    test('CreateRecipePage renders', () => {
+      render(<Router><CreateRecipePage/></Router>)
+    })
+});
