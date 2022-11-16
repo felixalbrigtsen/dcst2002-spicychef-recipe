a6c66e1c7e1e5684617754ffdd72a7d25b4ea42f
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-16 12:33:01 +0100

    Basic render done, need to mock with user #39 #30

--- client/src/test/pages/ShoppingListPage.test.tsx
+++ client/src/test/pages/ShoppingListPage.test.tsx
@@ -7,3 +7,18 @@ import {
 } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
+
+import ShoppingListPage from '../../pages/ShoppingListPage';
+
+describe('ShoppingListPage test', () => {
+    test('ShoppingListPage renders', () => {
+      render(<Router><ShoppingListPage/></Router>)
+      expect(screen.getByText('Shopping List')).toBeInTheDocument()
+      expect(screen.getByRole('button', {name: 'clearList'})).toBeInTheDocument()
+      expect(screen.getByRole('button', {name: 'clearList'})).toHaveTextContent('Clear all')
+      expect(screen.getByRole('table')).toBeInTheDocument()
+      expect(screen.getByRole('row')).toBeInTheDocument()
+      expect(screen.getByRole('columnheader', {name: "Item"})).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', {name: "Actions"})).toBeInTheDocument();
+    })
+});
