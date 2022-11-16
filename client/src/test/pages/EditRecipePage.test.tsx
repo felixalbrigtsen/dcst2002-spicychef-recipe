6c7510d5bdc53273b70e47619149373bebd71b83
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-16 12:31:54 +0100

    Almost done #30

--- client/src/test/pages/EditRecipePage.test.tsx
+++ client/src/test/pages/EditRecipePage.test.tsx
@@ -7,3 +7,11 @@ import {
 } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
+
+import EditRecipePage from '../../pages/EditRecipePage';
+
+describe('EditRecipePage test', () => {
+    test('EditRecipePage renders', () => {
+      render(<Router><EditRecipePage/></Router>)
+    })
+});
