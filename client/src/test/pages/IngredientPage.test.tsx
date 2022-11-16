e3f3db76b79141ea15d08b52579049ae7c24a7d3
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-16 12:29:57 +0100

    Renamed test page and added basic test #30

--- client/src/test/pages/IngredientPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Route, 
-  Routes, 
-  Link
-} from 'react-router-dom';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom'
