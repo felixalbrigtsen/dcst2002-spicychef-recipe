cb45bb9ac03d6f30a656c1f7320209c48200153c
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-11-16 10:36:22 +0100

    Fix broken Ingredient.ts

--- server/src/models/Ingredient.ts
+++ server/src/models/Ingredient.ts
@@ -5,19 +5,5 @@
  */
 export type Ingredient = {
   id: number;
-  ingredientName: string;
-
-  /**
-   * Number of units of the ingredient
-   * @example 1
-   */
-  quantity: number;
-  /**
-   * Unit ID from the table recipe_ingredient 
-   */
-  unitId: number;
-  /**
-   * Name of unit (dl, g, tbsp. etc.)
-   */
-  unitName: string;
+  name: string;
 };
