716ca7540ec5adfc4b70595076483037b6d4cf8c
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-11-16 12:32:08 +0100

    Basic done, import page is not complete #30

--- client/src/test/pages/ImportPage.test.tsx
+++ client/src/test/pages/ImportPage.test.tsx
@@ -7,3 +7,12 @@ import {
 } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
+
+import ImportPage from '../../pages/ImportPage';
+
+describe('ImportPage test', () => {
+    test('ImportPage renders', () => {
+      render(<Router><ImportPage/></Router>)
+      expect(screen.getByText('Import Page')).toBeInTheDocument()
+    })
+});
