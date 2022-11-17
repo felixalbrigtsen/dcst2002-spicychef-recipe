9b5a9a46ab57f4ae8aab0384524fb0a24bc8759b
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-11-17 13:46:30 +0100

    Export demo users

--- server/src/test/auth-router.test.ts
+++ server/src/test/auth-router.test.ts
@@ -15,13 +15,14 @@ const PORT = Number(process.env.PORT) || 3000;
 const jar = new CookieJar();
 export const axs = wrapper(axios.create({ jar }));
 
-const testUsers: User[] = [
+// Users for testing. 0 - normal user, 1 - admin user, 2 - invalid user
+export const testUsers: User[] = [
     {"googleId": "29130921380099" , "name": "testUser", "email": "testuser@example.com", "picture": "image1.jpg", "isadmin": false, "likes": [], "shoppingList": []},
     {"googleId": "89327493284798" , "name": "testAdmin", "email": "testadmin@example.com", "picture": "image2.jpg", "isadmin": true, "likes": [], "shoppingList": []},
     {"googleId": "-1" , "name": "invalid user", "email": "", "picture": "", "isadmin": false, "likes": [], "shoppingList": []}, // Automatically fails login because of the special googleId
 ]
 
-const testUserProfiles: UserProfile[] = testUsers.map(user =>
+export const testUserProfiles: UserProfile[] = testUsers.map(user =>
   ({
     id: user.googleId,
     displayName: user.name,
