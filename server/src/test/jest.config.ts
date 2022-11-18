3cc7046a517c4fc870a7265dc1e7c66ba5a3be2c
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-11-18 22:48:12 +0100

    Add verbose test output

--- server/src/test/jest.config.ts
+++ server/src/test/jest.config.ts
@@ -4,13 +4,13 @@ const config : Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '../.',
-  // testMatch: ["**/*.test.js", "**/*.test.tsx"],
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   coverageReporters: ["html", "text", "text-summary" ],
   transformIgnorePatterns: ["node_modules/(?!@testing-library)"],
   setupFiles: [
     "./test/dotenv-config"
   ],
+  verbose: true,
   maxWorkers: 1,
   collectCoverage: true,
   collectCoverageFrom: [
