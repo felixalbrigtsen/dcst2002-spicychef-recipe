66fa33f33df584155fd11882930c665704ac4f30
Jonas Haugland <jonashaugland@hotmail.com>
2022-11-18 18:30:45 +0100

    Added search bar to ingredientsPage, search is defective

--- client/src/pages/IngredientsPage.tsx
+++ client/src/pages/IngredientsPage.tsx
@@ -12,29 +12,38 @@ import {
   Container,
   Heading,
   Hero,
+  Icon,
   Notification,
   Table,
   Tile,
 } from "react-bulma-components";
 import { MdAddCircle } from "react-icons/md";
+import { mdiCarrot } from "@mdi/js";
 
 import { useAlert } from "../hooks/Alert";
 import { useLogin } from "../hooks/Login";
 
 export default function IngredientsPage() {
-  const [selectedIngredients, setSelectedIngredients] = React.useState<Ingredient[]>([]);
-  const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
-  const { appendAlert } = useAlert();
   const { user } = useLogin();
+  const { appendAlert } = useAlert();
+
+  const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
+  const [newQuery, setNewQuery] = React.useState<string>("");
+  const [selectedIngredients, setSelectedIngredients] = React.useState<Ingredient[]>([]);
+
+  let [visibleIngredients, setVisibleIngredients] = React.useState<Ingredient[]>([]);
 
   React.useEffect(() => {
     ingredientService
       .getIngredients()
       .then((ingredients) => {
         setIngredients(ingredients);
+        setVisibleIngredients(ingredients);
       })
       .catch((err) => console.error(err));
   }, []);
+
+  React.useEffect(() => {searchIngredients}, [newQuery]);
 
   function handleIngredientClick(ingredient: Ingredient) {
     if (selectedIngredients.includes(ingredient)) {
@@ -57,6 +66,25 @@ export default function IngredientsPage() {
     window.location.assign(`/search/?ingredients=${encodeURIComponent(ingredientIds.join(','))}`);
   }
 
+  function searchIngredients () {
+    if (newQuery.length > 0) {
+      console.log("searching for " + newQuery);
+      console.log(ingredients.filter((ingredient) => ingredient.name.toLowerCase().includes(newQuery.toLowerCase())))
+      setVisibleIngredients(ingredients.filter((ingredient) => ingredient.name.toLowerCase().includes(newQuery.toLowerCase())));
+    } else {
+      setVisibleIngredients(ingredients);
+    }
+  }
+
+  function filterVisibleIngredients(passingIngredients: Ingredient[]) {
+    if (newQuery.length > 0) {
+      passingIngredients = passingIngredients.filter((i) =>
+        i.name.toLowerCase().includes(newQuery.toLowerCase())
+      );
+      setVisibleIngredients(passingIngredients);
+    }
+  }
+
   return (
     <>
       <Container className="">
@@ -74,6 +102,24 @@ export default function IngredientsPage() {
               <Heading> Ingredients List </Heading>
             </Tile>
             <Box className="has-text-right">
+              <Form.Field>
+                <Form.Control
+                  className="has-icons-left"
+                >
+                  <Form.Input
+                    type="text"
+                    onChange={(event: React.FormEvent) => {
+                      {setNewQuery((event.target as HTMLInputElement).value), searchIngredients()};
+                    }}
+                    placeholder="Search for ingredients"
+                    value={newQuery}
+                  />
+                  <Icon
+                    className="is-left has-text-success"
+                    icon={mdiCarrot}
+                  />
+                </Form.Control>
+              </Form.Field>
               <Button
                 color="warning"
                 aria-label="searchAllIngredients"
@@ -115,7 +161,7 @@ export default function IngredientsPage() {
                   </tr>
                 </thead>
                 <tbody>
-                  {ingredients.map((ingredient, index) => (
+                  {visibleIngredients.map((ingredient, index) => (
                     <tr key={index}>
                       <td>{ingredient.name}</td>
                       <td className="is-narrow has-text-centered">
