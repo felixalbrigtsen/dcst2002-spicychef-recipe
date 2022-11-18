11560f234657c1aa10dc7872c3c5fcb94cfe0fbe
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-11-18 12:03:44 +0100

    Fix server test coverage, add debug output to pipeline tests

--- server/src/test/jest.config.ts
+++ server/src/test/jest.config.ts
@@ -3,19 +3,17 @@ import type {Config} from '@jest/types'
 const config : Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  rootDir: '../.',
   // testMatch: ["**/*.test.js", "**/*.test.tsx"],
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   coverageReporters: ["html", "text", "text-summary" ],
   transformIgnorePatterns: ["node_modules/(?!@testing-library)"],
   setupFiles: [
-    "./dotenv-config"
+    "./test/dotenv-config"
   ],
 
   maxWorkers: 1,
   collectCoverage: true,
-  collectCoverageFrom: [
-    "/src/**/*.ts",
-  ]
 }
 
 export default config
