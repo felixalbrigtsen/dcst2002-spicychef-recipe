c601703294fed4664ddc259fb9f977c0ba7d1f7e
Felix Albrigtsen <felixalb@stud.ntnu.no>
2022-11-19 16:34:21 +0100

    Add sorting to RecipeListPage

--- client/src/pages/RecipeListPage.tsx
+++ client/src/pages/RecipeListPage.tsx
@@ -9,24 +9,62 @@ import { Recipe } from "../models/Recipe";
 
 import { useLogin } from "../hooks/Login";
 
-import { Columns, Container, Button } from "react-bulma-components";
+import { Columns, Container, Button, Box, Form, Heading } from "react-bulma-components";
 import ScrollButton from "../components/ScrollUp";
 
 export default function RecipeList() {
-  let [recipeList, setRecipeList] = React.useState<Recipe[]>([]);
+  const [recipeList, setRecipeList] = React.useState<Recipe[]>([]);
+  const [sorting, setSorting] = useState("likes");
+
   const { user } = useLogin();
+
+  React.useEffect(() => {
+    let recipes = [...recipeList];
+
+    switch(sorting) {
+      case "likes":
+        recipes.sort((a, b) => b.likes - a.likes);
+        break;
+      case "newest":
+        recipes.sort((a, b) => b.id - a.id);
+        break;
+      case "oldest":
+        recipes.sort((a, b) => a.id - b.id);
+        break;
+
+      case "title":
+      default:
+        recipes.sort((a, b) => a.title.localeCompare(b.title));
+    }
+
+    setRecipeList(recipes);
+  }, [sorting]);
 
   React.useEffect(() => {
     recipeService.getRecipesShort().then((data) => {
       setRecipeList(data);
     });
   }, [user]);
+
   return (
     <>
       <Container
         className="mt-2 is-centered"
         style={{ margin: "auto" }}
       >
+        <Box>
+          <Form.Field kind="group" className="is-flex is-flex-direction-row is-vcentered is-justify-content-center">
+            <Heading className="is-flex-grow-1 is-expanded">Explore Recipes</Heading>
+
+            <Form.Label className="is-vcentered mr-1" style={{lineHeight: "2"}}> Sort by </Form.Label>
+            <Form.Select value={sorting} onChange={(event) => setSorting(event.target.value)}>
+              <option value="likes">Likes</option>
+              <option value="title">Title</option>
+              <option value="newest">Newest</option>
+              <option value="oldest">Oldest</option>
+            </Form.Select>
+          </Form.Field>
+        </Box>
         <Columns
           className="is-multiline"
           style={{ margin: "2rem auto" }}
